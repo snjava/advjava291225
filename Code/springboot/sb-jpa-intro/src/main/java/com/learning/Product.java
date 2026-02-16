2853b39d4865c3cc160076f2c79c7b2d50385b7e
snjava <scnaikwadi2@gmail.com>
2026-02-16 21:28:26 +0530

    Spring JPA Intro
    Spring JPA Intro

---
+++ Code/springboot/sb-jpa-intro/src/main/java/com/learning/Product.java
@@ -0,0 +1,46 @@
+package com.learning;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+//ERD - Entity Relational Diagram
+@Entity
+public class Product {
+    @Id
+    private int id;
+    private String title;
+    private int quantity;
+    private double price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
